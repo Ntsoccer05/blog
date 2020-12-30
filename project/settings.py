a16badd3bf52afa1fafbf20f87ae1a4d2d109188
Ntsoccer <2460socceronly0512@gmail.com>
2020-12-30 17:12:24 +0900

    AWS

--- project/settings.py
+++ project/settings.py
@@ -249,14 +249,14 @@ if not DEBUG:
     SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET']
     STRIPE_PUBLIC_KEY = os.environ['STRIPE_PUBLIC_KEY']
     STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
-    # AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    # AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     # S3のための記述
-    # AWS_STORAGE_BUCKET_NAME = 'blog-project'
-    # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-    # AWS_S3_OBJECT_PARAMETERS = {
-    #     'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
-    # }
-    # AWS_LOCATION = 'media'
-    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    AWS_STORAGE_BUCKET_NAME = 'takuya-blog'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
+    }
+    AWS_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
